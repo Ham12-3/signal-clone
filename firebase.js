2b6668c4bb1925ddcf611cc9704295930db2824d
Abdulhamid <mobolaji2309@gmail.com>
2023-09-01 18:16:19 +0100

    new commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDfEUhUQUMK4NvQ6j8VDUV6mUFkVmIw6dQ",
+  authDomain: "signal-clone-yt-40bef.firebaseapp.com",
+  projectId: "signal-clone-yt-40bef",
+  storageBucket: "signal-clone-yt-40bef.appspot.com",
+  messagingSenderId: "695306806552",
+  appId: "1:695306806552:web:2a006961140e36cc1f2a6d",
+};
