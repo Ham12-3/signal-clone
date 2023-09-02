ef28b8f363e948384b92f03c9be8c071a089f5ca
Abdulhamid <mobolaji2309@gmail.com>
2023-09-02 18:10:18 +0100

    updating

---
+++ screens/AddChatScreen.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import { Input, Button } from "@rneui/base";
+import { AntDesign } from "@expo/vector-icons";
+import { db } from "../firebase";
+
+const AddChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add a new Chat",
+      headerBackTitle: "Chats",
+    });
+  }, [navigation]);
+  const createChat = async () => {
+    await db
+      .collection("chats")
+      .add({
+        chatName: input,
+      })
+      .then(() => {
+        navigation.goBack();
+      })
+      .catch((error) => alert(error.message));
+  };
+  return (
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter a chat name"
+        value={input}
+        onChangeText={(text) => setInput(text)}
+        leftIcon={<AntDesign name="wechat" size={24} color="black" />}
+        onSubmitEditing={createChat}
+      />
+      <Button onPress={createChat} title="Create new chat" />
+    </View>
+  );
+};
+
+export default AddChatScreen;
+
+const styles = StyleSheet.create({
+  container: {},
+});
