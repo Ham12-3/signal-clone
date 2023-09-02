01ebd195a9331d82505d49812ee427d33e8c2ac9
Abdulhamid <mobolaji2309@gmail.com>
2023-09-03 00:29:40 +0100

    completed version 1

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -135,7 +135,7 @@ const ChatScreen = ({ navigation, route }) => {
                       <Text style={styles.receiverText}>{data.message}</Text>
                     </View>
                   ) : (
-                    <View style={styles.sender}>
+                    <View key={id} style={styles.sender}>
                       <Avatar
                         position="absolute"
                         bottom={-15}
